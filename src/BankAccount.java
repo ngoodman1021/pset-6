7368bc68f09612363e49672e3cadac48d1276c37
ngoodman1021 <32653505+ngoodman1021@users.noreply.github.com>
2020-01-21 09:00:39 -0500

    commit 21

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,16 +1,36 @@
-import.java.text.NumberFormat;
+import java.text.NumberFormat;
 
 public class BankAccount {
-        
-    private static long prevAccountNo = 100000000L;
+    
+	private static long prevAccountNo = 100000000L;
+	
     private int pin;
     private long accountNo;
     private double balance;
     private User accountHolder;
     
+    ////////////////////////////////////////////////////////////////////////////
+    //                                                                        //
+    // Refer to the Simple ATM tutorial to fill in the details of this class. //
+    //                                                                        //
+    ////////////////////////////////////////////////////////////////////////////
+    
+    /*
+     * Formats the account balance in preparation to be written to the data file.
+     * 
+     * @return a fixed-width string in line with the data file specifications.
+     */
+    
     private String formatBalance() {
-    	return String.format("%1$1s", balance);
+        return String.format("%1$15s", balance);
     }
+    
+    /*
+     * Converts this BankAccount object to a string of text in preparation to
+     * be written to the data file.
+     * 
+     * @return a string of text formatted for the data file
+     */
     
     @Override
     public String toString() {
@@ -27,27 +47,27 @@ public class BankAccount {
     
     public BankAccount(int pin, long accountNo, User accountHolder) {
     	this.pin = pin;
-    	this.accountNo = ++BankAccount.prevAccountNo; 
+    	this.accountNo = ++BankAccount.prevAccountNo;
     	this.accountHolder = accountHolder;
     }
     
     public BankAccount(int pin, long accountNo, double balance, User accountHolder) {
     	this.pin = pin;
-    	this.accountNo = ++BankAccount.prevAccountNo; 
+    	this.accountNo = ++BankAccount.prevAccountNo;
     	this.balance = balance;
     	this.accountHolder = accountHolder;
     }
     
-    
-    
+
     
     public int getPin() {
     	return pin;
     }
     
     public long getAccountNo() {
-    	return getAccountNo;
+    	return accountNo;
     }
+    
     
     public User getAccountHolder() {
     	return accountHolder;
@@ -55,25 +75,21 @@ public class BankAccount {
     
     public String getBalance() {
     	NumberFormat currency = NumberFormat.getCurrencyInstance();
+    	
     	return currency.format(balance);
     }
     
     public int deposit(double amount) {
-    	if (amount <= 0) {
+    	if(amount <= 0) {
     		return ATM.INVALID;
-    	
-    	}else if(balance + amount >= ATM.INVALIDTOP){
+    	} else if (balance + amount >= ATM.INVALIDTOP) {
     		return ATM.INVALIDMAX;
-    	
-    	}else if(balance + amount > balance) {
-    		return ATM.INSUFFICIENT;
-    	
-    	}else{
+    	} else {
     		balance = balance + amount;
     	}
     	return ATM.SUCCESS;
     }
-	
+    
     public int transfer(double amount, String wOD) {
     	if(amount <= 0) {
     		return ATM.INVALID;
@@ -90,13 +106,13 @@ public class BankAccount {
     	}
     	return ATM.SUCCESS;
     }
-    
+   
     public int withdraw(double amount) {
     	if(amount <= 0) {
     		return ATM.INVALID;
-    	}else if(amount > balance){
+    	} else if (amount > balance){
     		return ATM.INSUFFICIENT;
-    	}else {
+    	} else {
     		balance = balance - amount;
     	}
     	return ATM.SUCCESS;
